fad738ecab0ab6e14cc98e0ac5d703a649933462
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2021-09-24 08:11:20 -0700

    Add an xs breakpoint in the tailwind config file. Add new header nav styles for packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -197,6 +197,7 @@ module.exports = {
             "100": "100vh",
         },
         screens: {
+            xs: { max: "639px" },
             sm: "640px",
             md: "768px",
             lg: "1024px",
