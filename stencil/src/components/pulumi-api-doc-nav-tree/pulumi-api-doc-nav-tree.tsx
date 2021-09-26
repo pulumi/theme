52c7150b2949aade4bb3a6baeba2200eb50e3020
Kimberley Mackenzie <kimberley@pulumi.com>
2021-09-26 15:06:03 -0700

    Add filter logic to the tree nav, clean up styles.

--- stencil/src/components/pulumi-api-doc-nav-tree/pulumi-api-doc-nav-tree.tsx
+++ stencil/src/components/pulumi-api-doc-nav-tree/pulumi-api-doc-nav-tree.tsx
@@ -1,55 +1,36 @@
-import { Component, h, Prop } from '@stencil/core';
-
-export interface node {
-    name: string,
-    type: "module" | "function" | "resource",
-    link: string,
-    children?: node[], 
-    isExpanded?: boolean;
-}
+import { Component, h, Prop } from "@stencil/core";
+import { node } from "../pulumi-api-doc-filterable-nav/pulumi-api-doc-filterable-nav";
 
 @Component({
-  tag: 'pulumi-api-doc-nav-tree',
-  styleUrl: 'pulumi-api-doc-nav-tree.scss',
-  shadow: true,
+    tag: "pulumi-api-doc-nav-tree",
+    styleUrl: "pulumi-api-doc-nav-tree.scss",
+    shadow: true,
 })
-
 export class PulumiApiDocNavTree {
- @Prop()
-  nodes: node[];
+    @Prop()
+    nodes: node[];
 
-  getIcon(nodeType) {
-    return <slot name="before-content"><span class={ `symbol ${nodeType}` }></span></slot>
-  }
+    getIcon(nodeType) {
+        return (
+            <slot name="before-content">
+                <span class={`symbol ${nodeType}`}></span>
+            </slot>
+        );
+    }
 
-  getChildNodes() {
-      return this.nodes.map(node =>
-        
-        <pulumi-tree-item slot="item" expanded={ `${!!node.isExpanded}` } class="nav-tree-item">{this.getIcon(node.type)}<slot name="content"><a href={ `${node.link}`}>{node.name}</a></slot>
-        
-        {node.children && node.children.map(childNode => 
-        <pulumi-tree-item slot="item" expanded={ `${!!node.isExpanded}` } class="nav-tree-item">{this.getIcon(childNode.type)}<slot name="content"><a href={ `${childNode.link}`}>{childNode.name}</a></slot>
+    getChildNodes(nodes: node[] = this.nodes) {
+        return nodes.map((node) => (
+            <pulumi-tree-item slot="item" expanded={!!node.isExpanded} class="nav-tree-item">
+                {this.getIcon(node.type)}
+                <slot name="content">
+                    <a href={`${node.link}`}>{node.name}</a>
+                </slot>
+                {node.children && this.getChildNodes(node.children)}
+            </pulumi-tree-item>
+        ));
+    }
 
-
-                {childNode.children && childNode.children.map(nextNode => 
-        <pulumi-tree-item slot="item" expanded={ `${!!node.isExpanded}` } class="nav-tree-item">{this.getIcon(nextNode.type)}<slot name="content"><a href={ `${nextNode.link}`}>{nextNode.name}</a></slot></pulumi-tree-item>
-        )}
-        
-        
-        </pulumi-tree-item>
-        )}
-
-        </pulumi-tree-item>
-      )
-  }
-
-
-  render() {
-    return (
-        <pulumi-tree-view>
-          {this.getChildNodes()}
-        </pulumi-tree-view>
-    );
-  }
-
+    render() {
+        return <pulumi-tree-view>{this.getChildNodes()}</pulumi-tree-view>;
+    }
 }
