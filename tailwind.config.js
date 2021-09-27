3381eea9a6effdf1ea1417a054c78624a49df0d1
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2021-09-27 13:36:25 -0700

    Remove xs tailwind breakpoint. Update logo nav menu with a default mobile viewport assumption

--- tailwind.config.js
+++ tailwind.config.js
@@ -198,7 +198,6 @@ module.exports = {
             "100": "100vh",
         },
         screens: {
-            xs: { max: "639px" },
             sm: "640px",
             md: "768px",
             lg: "1024px",
