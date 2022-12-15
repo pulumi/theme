5fc7d652c65b877047d455f6a9094732594faed0
Sean Holung <sean.holung@gmail.com>
2022-12-15 15:15:07 -0800

    wip

--- stencil/src/components/resource-links/resource-links.tsx
+++ stencil/src/components/resource-links/resource-links.tsx
@@ -21,38 +21,38 @@ export class ResourceLinks {
     relatedResources: {};
 
     componentWillLoad() {
-        fetch("https://gist.githubusercontent.com/sean1588/516dc164f8705572db7eb7c3420ec3ab/raw/e88fd819e05a78fe1d8858203098ada8604c7ca7/related-resources.json")
+        fetch("https://www.pulumi.com/uploads/related-resources/2022-12-15.json")
         .then(resp => resp.json())
         .then((response) => {
-            console.log(response)
-            this.relatedResources = response
+            this.relatedResources = response;
         })
+        .catch(err => console.error(err))
     }
 
     render() {
         if (!this.relatedResources) {
             return <div></div>
         }
-        const provider = this.packageName.toLowerCase();
+        const pkg = this.packageName.toLowerCase();
         const module = this.moduleName.toLowerCase();
         const typ = this.resourceName.toLowerCase();
         
-        const related = this.relatedResources[provider][module][typ];
+        const related = this.relatedResources[pkg][module][typ];
 
         if (!related) {
             return <div></div>
         }
 
         return (
-            <div class="container" style={{display: ""}}>
+            <div class="container">
                 <div class="heading">Related Resources</div>
                     {
                         related.map(t => {
                             const parts = t.split("/");
-                            const res = parts[parts.length-1];
-                            const module = parts.slice(0, parts.length-1).join("/");
-                            const rellink = parts.slice(0, parts.length).map(_ => "../").join("");
-                            return <div class="links"><a href={`${rellink}${module.toLowerCase()}`}>{`${module}`}</a> / <a href={`${rellink}${t.toLowerCase()}`}>{`${res}`}</a></div>
+                            let module = parts.slice(0, parts.length-1).join("/");
+                            const resource = parts[parts.length-1];
+                            const rellink = `/registry/packages/${pkg}/api-docs/${module}/${resource.toLowerCase()}/`;
+                            return <div class="links"><a href={rellink}>{`${module}`}.{`${resource}`}</a></div>
                         })
                     }
             </div>
