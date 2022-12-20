8de94b04f077538cf3b58a4ad51cee293bb7a548
Sean Holung <sean.holung@gmail.com>
2022-12-19 16:56:29 -0800

    clean up

--- stencil/src/components/resource-links/resource-links.tsx
+++ stencil/src/components/resource-links/resource-links.tsx
@@ -18,14 +18,23 @@ export class ResourceLinks {
     resourceName: string;
 
     @State()
-    relatedResources: {};
+    related: [];
 
     componentWillLoad() {
         // Fetch JSON file containing resource link information.
         fetch("https://www.pulumi.com/uploads/related-resources/2022-12-19.json")
         .then(resp => resp.json())
         .then((response) => {
-            this.relatedResources = response;
+            const relatedResources = response;
+            if (relatedResources) {
+                const pkg = this.packageName.toLowerCase();
+                const module = this.moduleName.toLowerCase();
+                const typ = this.resourceName.toLowerCase();
+                
+                // Look up related links for this resource.
+                this.related = relatedResources[pkg] ? relatedResources[pkg][module] ? relatedResources[pkg][module][typ] : undefined : undefined;
+
+            }
         })
         .catch(err => console.error(err))
     }
@@ -33,29 +42,19 @@ export class ResourceLinks {
     render() {
         // relatedResources will be set once the response returns and state is updated,
         // so return empty div until then.
-        if (!this.relatedResources) {
+        if (!this.related) {
             return <div></div>
         }
         const pkg = this.packageName.toLowerCase();
-        const module = this.moduleName.toLowerCase();
-        const typ = this.resourceName.toLowerCase();
-        
-        // Look up related links for this resource.
-        const related = this.relatedResources[pkg] ? this.relatedResources[pkg][module] ? this.relatedResources[pkg][module][typ] : undefined : undefined;
-
-        if (!related) {
-            return <div></div>
-        }
-
         return (
             <div class="container">
                 <div class="heading">Related Resources</div>
                     {
-                        related.map(t => {
+                        this.related.map((typ: string) => {
                             // Parse out the module and resource from the related resource.
                             // We are currently only linking to resources related in the
                             // same package.
-                            const parts = t.split("/");
+                            const parts = typ.split("/");
                             let module = parts.slice(0, parts.length-1).join("/");
                             const resource = parts[parts.length-1];
                             const rellink = `/registry/packages/${pkg}/api-docs/${module}/${resource.toLowerCase()}/`;
